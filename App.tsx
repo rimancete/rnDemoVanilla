c64d48737659fee2625de36252e06b799224155b
Osvaldo Costa <osvaldo.costajr@gmail.com>
2023-11-18 12:09:53 -0300

    S13L229 - Non-Expo apps & native device features

---
+++ App.tsx
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View style={styles.screen}>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
